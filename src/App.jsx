48559788ddb0425f74a9f3ebd289a9200eb73674
vivek0902 <viveksharma090297@gmail.com>
2025-03-29 21:16:36 +0530

    added left right up down movement using keyboard arrow button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const SIZE = 4;
+
 const genetateEmptyBoard = () => {
   return [
     [2, 2, 0, 0],
@@ -10,10 +11,76 @@ const genetateEmptyBoard = () => {
     [8, 8, 16, 16],
   ];
 };
+
+const mergeRow = (row) => {
+  let newRow = row.filter((cell) => cell !== 0);
+
+  for (let i = 0; i < newRow.length; i++) {
+    if (newRow[i] === newRow[i + 1]) {
+      newRow[i] = newRow[i] * 2;
+      newRow[i + 1] = 0;
+    }
+  }
+  newRow = newRow.filter((cell) => cell !== 0);
+
+  while (newRow.length < SIZE) {
+    newRow.push(0);
+  }
+
+  return newRow;
+};
+
+const transpose = (board) => {
+  let newBoard = [];
+  for (let c = 0; c < board[0].length; c++) {
+    let newRow = [];
+    for (let r = 0; r < board.length; r++) {
+      newRow.push(board[r][c]);
+    }
+    newBoard.push(newRow);
+  }
+  return newBoard;
+};
+
+const leftMove = (board) => board.map((row) => mergeRow(row));
+
+const rightMove = (board) =>
+  board.map((row) => mergeRow(row.reverse()).reverse());
+
+const upMove = (board) => transpose(leftMove(transpose(board)));
+
+const downMove = (board) => transpose(rightMove(transpose(board)));
+
 function App() {
   const [board, setBoard] = useState(() => {
     return genetateEmptyBoard();
   });
+
+  useEffect(() => {
+    const handleGameControlKey = (e) => {
+      let newBoard;
+      switch (e.key) {
+        case "ArrowLeft":
+          newBoard = leftMove(board);
+          break;
+        case "ArrowRight":
+          newBoard = rightMove(board);
+          break;
+        case "ArrowUp":
+          newBoard = upMove(board);
+          break;
+        case "ArrowDown":
+          newBoard = downMove(board);
+          break;
+        default:
+          return;
+      }
+      setBoard(newBoard);
+    };
+
+    window.addEventListener("keyup", handleGameControlKey);
+    return () => window.removeEventListener("keyup", handleGameControlKey);
+  }, [board]);
 
   return (
     <div className="container">
