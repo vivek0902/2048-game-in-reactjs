90686c3b9cb617ed9788c8a60901465c81318f72
vivek0902 <viveksharma090297@gmail.com>
2025-03-30 09:06:45 +0530

    update package.json and vir=te config to deploy on github by using gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "2048-game-in-reactjs",
   plugins: [react()],
-})
+});
